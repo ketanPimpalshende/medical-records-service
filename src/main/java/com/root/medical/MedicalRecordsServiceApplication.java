8506636a3906fbf4dd2cd5aa617af1e9c8ceb2b5
Ketan Pimpalshende <ketanpimpalshende@gmail.com>
2024-06-09 13:45:09 +0530

    initial setup

---
+++ src/main/java/com/root/medical/MedicalRecordsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.root.medical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalRecordsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalRecordsServiceApplication.class, args);
+	}
+
+}
